e8f0dbeb7a93fcf27ff235782fad9f44a1f319ab
PrayagPVT <anujpatellive@gmail.com>
2024-06-30 12:40:02 +0000

    comparsion done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,36 @@
+// console.log(2 < 1);
+// console.log(2>=1);
+// console.log(2 < 1);
+// console.log(2 ==1 );
+// console.log(2 != 1);
+// this are basic conversion 
+
+
+// yaha 2 different type ke data structure ko compare kiya hai  
+console.log("2" > 1);
+console.log("02" > 1);
+// dono ka ans false aya hai 
+// jabb 2 data structure ko compare krte hai hai output alag ane ke chances hote hai 
+
+console.log(null > 0);
+console.log(null == 0);
+// upar ke dono ka output false hai 
+console.log(null >= 0);
+// iska output true hai kyuki jab comparison hua toh null 0 me convert ho gaya aur 0 equal hai 0 ke 
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// teno ki value false hai 
+
+// STRICT CHECK ISSE JIS BHI CHIZ KA COMPARISON KRTE HAI USKA DATA STRUCTURE BHI
+//  CHECK HOTA HAI YEH (===) YEH 3 EQUAL  TO SE CHECK HOTA HAI NICHE ISKA EXAMPLE HAI 
+
+console.log("2" == 2);
+// iska output true aega kyuki string yaha convert ho jaegi number me 
+
+console.log("2" === 2);
+// yaha strict check hua hai iska answer false aega kyuki yeh data structure bhi check hoga 
+
+
+// ******************************************BASIC KO CHORDKE BAKI SAB  COMPARISON KO AVOID KRNA HAI ***************************
