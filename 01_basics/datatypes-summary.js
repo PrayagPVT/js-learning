718603d684d8711174e87da840563139151bd62b
PrayagPVT <anujpatellive@gmail.com>
2024-07-01 10:54:38 +0000

    Summary done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+// JavaScript is dynamically typed language 
+// js is dynamically typed language because
+// example 
+const score = 100 
+//  (yaha apan ne bataya nhi ki value numbers me hai fer bhi javascript ko pata hai )
+// kis tarah se data ko memory me rakha jata hai aur access kiya jata hai us basis pe data ke 2 types hai 
+
+// Primitive
+// There are 7 types of primitive data types (all primitive data types are call by value isme data ki copy banti hai )
+// 1 => String
+// 2 => number
+// 3 => null
+// 4 => Boolean
+// 5 => undefined (variable declare hai par abhi value nhi di hai )
+// 6 => Symbol (kisi bhi value ko unique banane ke liye symbol use kiy ajata hai )
+// How to declare symbol
+  const id = symbol('123')
+// 7 => BigInt (integer se badi value ko store krne ke liye big int use hota hai ) 
+
+// Non-primitive (reference type)
+// 1 => Array 
+// Example of array =>
+    const heroes = ["shaktiman", "naagrraj" , "doga"]
+//  arrays ko square bracket me likhte hai 
+// 2 => Objects
+// Example of objects 
+      let myObj = {
+        name :"prayag",
+        age : 22,
+      }
+//  kurli braces ke andar likhte hai key value pair me 
+// 3 => Functions 
+// Example of function => 
+const myFunction = function(){
+    console.log("hello world");
+}
+
+
+// Return typeof of variables in java script
+
+// PRIMITIVE DATA TYPES
+// 1 => String => string
+// 2 => number => number
+// 3 => null => object
+// 4 => Boolean => boolean
+// 5 => undefined => undefined
+// 6 => Symbol => symbol
+// 7 => BigInt => bigint
+
+// NON-PRIMITIVE TYPE 
+// 1 => arrays => Object
+// 2 => Function => Function 
+// 3 => object => object
