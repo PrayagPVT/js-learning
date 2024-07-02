a7ea9421e9ad727b9f536a0954d866ec838862be
PrayagPVT <anujpatellive@gmail.com>
2024-07-02 13:47:36 +0000

    Heap and Stack memory done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,7 +14,7 @@ const score = 100
 // 5 => undefined (variable declare hai par abhi value nhi di hai )
 // 6 => Symbol (kisi bhi value ko unique banane ke liye symbol use kiy ajata hai )
 // How to declare symbol
-  const id = symbol('123')
+  // const id = symbol('123')
 // 7 => BigInt (integer se badi value ko store krne ke liye big int use hota hai ) 
 
 // Non-primitive (reference type)
@@ -51,3 +51,35 @@ const myFunction = function(){
 // 1 => arrays => Object
 // 2 => Function => Function 
 // 3 => object => object
+
+
+
+// *****************************************************************************MEMORY*********************************
+// STACK primitive me use hoti hai (copy milta hai)
+// HEAP (non primitive) (reference milti hai)
+
+let myNameIs = "PrayagPatel"
+let AnotherName  = myNameIs
+AnotherName = "Anujpatel"
+
+console.log(AnotherName)
+console.log(myNameIs)
+
+// abb yaha pehle ka output Anujpatel ara hai aur dusre ka prayag patel kyuki
+//  anothername ki ek alag copy ban gai hai aur uske andar value store hai isiliye isko stack memory bolenge
+
+let userOne = {
+  email: "prayag@gmail.com",
+  upi : "gpay"
+}
+
+let userTwo = userOne
+userTwo.email = "anuj2gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
+// yaha dono ke andar anuj2gmail aega kyuki reference milga heap memory ka 
+
+// At the end yeh bol skte hai ki Stack memory me copy banti hai jo ki change ho skti
+//  hai lekin Heap memory me reference milta hai jo ki change nhi ho sklta hai  
