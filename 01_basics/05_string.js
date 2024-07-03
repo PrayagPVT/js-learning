6dfd02c48f5faea5af1af14b9d6e4621271c32f1
PrayagPVT <anujpatellive@gmail.com>
2024-07-03 15:07:41 +0000

    half work done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "Prayag"
+const repoCount = 25
+
+// console.log(name + repoCount + " value"); 
+// yeh upar wala utna khas nhi hia jayada use nhi krna hai niche wala use krna hai 
+
+// console.log(`Hello my name is ${name} and my birthdate is ${repoCount}`);
+// upar wale ko string interpulation bola jata hai 
+
+const gameName = new string('Prayag')
+// is type se bhi string declare kr skte hai 
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUppercase());
