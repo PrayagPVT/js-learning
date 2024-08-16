648d15ec519b382b61f99133fe17a1d0c68026d4
PrayagPVT <anujpatellive@gmail.com>
2024-08-16 09:55:35 +0000

    advanced_array

---
+++ 10_funwith_js/araayAdvanced.js
@@ -0,0 +1,10 @@
+const myArr = []
+%DebubPrint(myArr)
+
+// array are of two types 
+// continiou, Holey (holley mtlbb hole)
+
+// optimization js me 3 type se hote hai 
+// SMI (small integer)
+//  packed element
+// Double (float, string, function)
